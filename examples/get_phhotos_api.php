a4bf809e4dae54711da5e3cf1b1bb189b674d7c4
ansu shah <ansuangry@ymail.com>
2014-10-18 06:53:15 +0000

    get photos api added

--- examples/get_phhotos_api.php
+++ examples/get_phhotos_api.php
@@ -3,6 +3,9 @@
 require '../src/facebook.php';
 require '../parse/parse.php';
 
+$parseQuery = new parseQuery();
+var_dump($parseQuery);
+
 $parseObject = new parseObject('fb_photos');
 var_dump($parseObject);
 ?>
